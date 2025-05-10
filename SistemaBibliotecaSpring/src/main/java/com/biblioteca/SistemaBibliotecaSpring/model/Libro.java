95e5f80a151f8f319c17760ee5ea332390654fb2
celi <celinaguerradiaz@gmail.com>
2025-05-10 17:18:54 -0300

    añado clase libro

---
+++ SistemaBibliotecaSpring/src/main/java/com/biblioteca/SistemaBibliotecaSpring/model/Libro.java
@@ -0,0 +1,19 @@
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Libro {
+    private Long id;
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private EstadoLibro estado;
+}
+
+public enum EstadoLibro {
+    DISPONIBLE,
+    PRESTADO
+}
