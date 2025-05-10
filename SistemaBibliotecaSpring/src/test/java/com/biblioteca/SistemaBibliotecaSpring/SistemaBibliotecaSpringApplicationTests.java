65530640af754a1769a0983b52733d45e64c2fbe
celi <celinaguerradiaz@gmail.com>
2025-05-10 17:07:10 -0300

    añado config spring boot

---
+++ SistemaBibliotecaSpring/src/test/java/com/biblioteca/SistemaBibliotecaSpring/SistemaBibliotecaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.SistemaBibliotecaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBibliotecaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
